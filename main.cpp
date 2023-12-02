1798fdb26ceade753bd40500754b76a6d9a27d40
SerZok <SerZok.021@gmail.com>
2023-12-02 12:21:26 +0300

    9 start

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 #include "Display.h"
 #include "Simulation.h"
 
+#include "Il/il.h"
+#include "Il/ilu.h"
+#include "Il/ilut.h"
+
 using namespace std;
 
 // функция, вызываемая при изменении размеров окна
@@ -23,7 +27,36 @@ void reshape(int w, int h){
 int main(int argc, char** argv){
 	setlocale(LC_ALL, "ru");
 
-	// инициализация библиотеки GLUT
+	// инициализация библиотеки DevIL 
+	ilInit();
+	iluInit();
+	ilutInit();
+
+	// создаем новое "изображение"
+	ILuint imageId = ilGenImage();
+	// задаем текущее "изображение"
+	ilBindImage(imageId);
+	// удаляем "изображением"
+	ilDeleteImage(imageId);
+
+
+	// загружаем изображение
+	string fileName = "data\\textures\\plane.jpg";
+	wchar_t unicodeString[256];
+	wsprintf(unicodeString, L"%S", fileName.c_str());
+	bool result = ilLoadImage(unicodeString);
+
+	// получение параметров загруженной текстуры
+	int width = ilGetInteger(IL_IMAGE_WIDTH);
+	int height = ilGetInteger(IL_IMAGE_HEIGHT);
+	int format = ilGetInteger(IL_IMAGE_FORMAT);
+	int type = ilGetInteger(IL_IMAGE_TYPE);
+
+	cout << "W:" << width << endl << "H:" << height << endl << "F:" << format << endl << "T: " << type << endl;
+
+	ilCopyPixels();
+
+		// иниtype =циализация библиотеки GLUT
 	glutInit(&argc, argv);
 	// инициализация дисплея (формат вывода)
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
